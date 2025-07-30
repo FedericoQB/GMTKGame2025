a06b540d269869f449ec80416a507758668f2311
FedericoQB <federico.quijano@outlook.com>
2025-07-30 21:47:46 +0200

    Added buttons, interactions. Fixed camera

---
+++ Assets/Federico/WorldScripts/PhysicsInteractScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsInteractScript : MonoBehaviour
+{
+    public bool IsOnTop;
+    public UnityEvent pushedDownAction; // Pushed down and event will Invoke
+    public UnityEvent pushedUpAction; // Pushed up and event will invoke
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOnTop)
+        {
+            pushedDownAction.Invoke();
+        }
+        else
+        {
+            pushedUpAction.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Shadow")
+        {
+            IsOnTop = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Shadow")
+        {
+            IsOnTop = false;
+        }
+    }
+}
