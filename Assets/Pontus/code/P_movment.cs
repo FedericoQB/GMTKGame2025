9246cb78adabb511b4ce413ee77bc16afce69164
POKL67 <PONTUS.KLINT@GMAIL.COM>
2025-07-31 21:31:57 +0200

    aaaaaaaaa

--- Assets/Pontus/code/P_movment.cs
+++ Assets/Pontus/code/P_movment.cs
@@ -11,9 +11,10 @@ public static class Tracking
 {
     public static  List<int> tracking = new List<int>();
     public static int moves;
-    public static int maxMoves = 100000;
+    public static int maxMoves = 10;
     public static int shadowOrder = 1;
     public static int currentShadow = 1;
+    public static int boxMov = 0;
 }
 
 public class P_movment : MonoBehaviour
@@ -25,7 +26,7 @@ public class P_movment : MonoBehaviour
     private bool isDiagonal = false;
     private float xPos;
     private float yPos;
-
+    private bool hasSpawnedShadow = false;
 
     public Vector2 movement;
     public Rigidbody2D rb;
@@ -33,7 +34,12 @@ public class P_movment : MonoBehaviour
     Vector2 mousePos;
     public GameObject Player;
     private Vector2 laserV2;
+    private Vector2 ChestlaserV2;
     public TextMeshProUGUI Moves;
+
+    public GameObject Chest;
+    private float ChestxPos;
+    private float ChestyPos;
 
     public GameObject shadow;
     private void Start()
@@ -43,109 +49,164 @@ public class P_movment : MonoBehaviour
         movementSpeed07 = speed * 0.6f;  // Set diagonal movement speed (60% of the normal speed)
         Tracking.moves = Tracking.maxMoves;
         Moves.text = Tracking.moves.ToString();
-    }
 
+    }
     private void Update()
     {
         xPos = Mathf.RoundToInt(Player.transform.position.x);
         yPos = Mathf.RoundToInt(Player.transform.position.y);
+        ChestxPos = Mathf.RoundToInt(Chest.transform.position.x);
+        ChestyPos = Mathf.RoundToInt(Chest.transform.position.y);
 
-
+        // W = Up
         if (Input.GetKeyDown(KeyCode.W) && Tracking.moves > 0)
         {
             laserV2 = new Vector2(xPos, yPos + 1);
-            Collider2D hit = Physics2D.OverlapPoint(laserV2, LayerMask.GetMask("ground"));
-            if (hit == null || !hit.CompareTag(Tags.Wall))
+            Collider2D hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+            if (hit != null && hit.CompareTag("Chest"))
             {
-                Player.transform.position = new Vector2(xPos, yPos + 1);
-                Player.transform.rotation = Quaternion.Euler(0, 0, 0);
-                Tracking.moves--;
-                Moves.text = Tracking.moves.ToString();
-                Tracking.tracking.Add(1);
-            }
-            else
-            {
-                Collider2D hitChest = Physics2D.OverlapPoint(laserV2, LayerMask.GetMask("ground"));
-                if (hitChest.CompareTag("Chest")) 
+                ChestlaserV2 = new Vector2(xPos, yPos + 2);
+                Collider2D hitWall = Physics2D.OverlapPoint(ChestlaserV2/*, LayerMask.GetMask("ground")*/);
+
+                if (hitWall == null || !hitWall.CompareTag(Tags.Wall))
                 {
-                
+                    hit.transform.position = ChestlaserV2;
+                    Player.transform.position = new Vector2(xPos, yPos + 1);
+                    Player.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    Tracking.moves--;
+                    Tracking.tracking.Add(1);
+                    Chest.transform.position = new Vector2(ChestxPos, ChestyPos + 1);
+                    Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
                 }
             }
+            else if (hit == null || !hit.CompareTag(Tags.Wall))
+            {
+                Player.transform.position = new Vector2(xPos, yPos + 1);
+                Player.transform.rotation = Quaternion.Euler(0, 0, 270);
+                Tracking.moves--;
+                Tracking.tracking.Add(1);
+            }
+            Moves.text = Tracking.moves.ToString();
         }
+        // S = Down
         if (Input.GetKeyDown(KeyCode.S) && Tracking.moves > 0)
         {
             laserV2 = new Vector2(xPos, yPos - 1);
-            Collider2D hit = Physics2D.OverlapPoint(laserV2, LayerMask.GetMask("ground"));
-            if (hit == null || !hit.CompareTag(Tags.Wall))
+            Collider2D hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+            if (hit != null && hit.CompareTag("Chest"))
+            {
+                ChestlaserV2 = new Vector2(xPos, yPos - 2);
+                Collider2D hitWall = Physics2D.OverlapPoint(ChestlaserV2/*, LayerMask.GetMask("ground")*/);
+
+                if (hitWall == null || !hitWall.CompareTag(Tags.Wall))
+                {
+                    hit.transform.position = ChestlaserV2;
+                    Player.transform.position = new Vector2(xPos, yPos - 1);
+                    Player.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    Tracking.moves--;
+                    Tracking.tracking.Add(2);
+                    Chest.transform.position = new Vector2(ChestxPos, ChestyPos - 1);
+                    Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                }
+            }
+            else if (hit == null || !hit.CompareTag(Tags.Wall))
             {
                 Player.transform.position = new Vector2(xPos, yPos - 1);
-                Player.transform.rotation = Quaternion.Euler(180, 0, 0);
+                Player.transform.rotation = Quaternion.Euler(0, 0, 90);
                 Tracking.moves--;
-                Moves.text = Tracking.moves.ToString();
                 Tracking.tracking.Add(2);
             }
-            else
-            {
-                /*
-                Player.transform.position = new Vector2(xPos, yPos - 1);
-                Player.transform.rotation = Quaternion.Euler(180, 0, 0);
-                moves--;
-                Moves.text = moves.ToString();
-                */
-            }
+            Moves.text = Tracking.moves.ToString();
         }
+
+        // D = Right
         if (Input.GetKeyDown(KeyCode.D) && Tracking.moves > 0)
         {
             laserV2 = new Vector2(xPos + 1, yPos);
-            Collider2D hit = Physics2D.OverlapPoint(laserV2, LayerMask.GetMask("ground"));
-            if (hit == null || !hit.CompareTag(Tags.Wall))
+            Collider2D hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+            if (hit != null && hit.CompareTag("Chest"))
+            {
+                ChestlaserV2 = new Vector2(xPos + 2, yPos);
+                Collider2D hitWall = Physics2D.OverlapPoint(ChestlaserV2/*, LayerMask.GetMask("ground")*/);
+
+                if (hitWall == null || !hitWall.CompareTag(Tags.Wall))
+                {
+                    hit.transform.position = ChestlaserV2;
+                    Player.transform.position = new Vector2(xPos + 1, yPos);
+                    Player.transform.rotation = Quaternion.Euler(0, 0, 270);
+                    Tracking.moves--;
+                    Tracking.tracking.Add(3);
+                    Chest.transform.position = new Vector2(ChestxPos + 1, ChestyPos);
+                    Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                }
+            }
+            else if (hit == null || !hit.CompareTag(Tags.Wall))
             {
                 Player.transform.position = new Vector2(xPos + 1, yPos);
                 Player.transform.rotation = Quaternion.Euler(0, 0, 270);
                 Tracking.moves--;
-                Moves.text = Tracking.moves.ToString();
                 Tracking.tracking.Add(3);
             }
-            else
-            {
-                /*
-                Player.transform.position = new Vector2(xPos+1, yPos);
-                Player.transform.rotation = Quaternion.Euler(180, 0, 0);
-                moves--;
-                Moves.text = moves.ToString();
-                */
-            }
+
+            Moves.text = Tracking.moves.ToString();
         }
+        // A = Left
         if (Input.GetKeyDown(KeyCode.A) && Tracking.moves > 0)
         {
             laserV2 = new Vector2(xPos - 1, yPos);
-            Collider2D hit = Physics2D.OverlapPoint(laserV2, LayerMask.GetMask("ground"));
-            if (hit == null || !hit.CompareTag(Tags.Wall))
+            Collider2D hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+            if (hit != null && hit.CompareTag("Chest"))
+            {
+                ChestlaserV2 = new Vector2(xPos - 2, yPos);
+                Collider2D hitWall = Physics2D.OverlapPoint(ChestlaserV2/*, LayerMask.GetMask("ground")*/);
+
+                if (hitWall == null || !hitWall.CompareTag(Tags.Wall))
+                {
+                    hit.transform.position = ChestlaserV2;
+                    Player.transform.position = new Vector2(xPos - 1, yPos);
+                    Player.transform.rotation = Quaternion.Euler(0, 0, 90);
+                    Tracking.moves--;
+                    Tracking.tracking.Add(4);
+                    Chest.transform.position = new Vector2(ChestxPos - 1, ChestyPos);
+                    Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                }
+            }
+            else if (hit == null || !hit.CompareTag(Tags.Wall))
             {
                 Player.transform.position = new Vector2(xPos - 1, yPos);
                 Player.transform.rotation = Quaternion.Euler(0, 0, 90);
                 Tracking.moves--;
-                Moves.text = Tracking.moves.ToString();
                 Tracking.tracking.Add(4);
             }
-            else
-            {
-                /*
-                Player.transform.position = new Vector2(xPos - 1, yPos);
-                Player.transform.rotation = Quaternion.Euler(180, 0, 0);
-                moves--;
-                Moves.text = moves.ToString();
-                */
-            }
+
+            Moves.text = Tracking.moves.ToString();
         }
 
-        if (Tracking.moves == 0) 
+        // When moves run out, spawn the shadow and reset
+        if (Tracking.moves == 0 && !hasSpawnedShadow)
         {
-            //Player.transform.position = new Vector2 (ExitStuff.emptyExitGlobal.position.x,ExitStuff.emptyExitGlobal.position.y);
-            Player.transform.position = new Vector2 (0,0);
+            hasSpawnedShadow = true;
+            Player.transform.position = new Vector2(0, 0); // Optional: move player to reset point
             Instantiate(shadow, Player.transform.position, Player.transform.rotation);
             Tracking.shadowOrder++;
-            Tracking.moves = Tracking.maxMoves;
+           
+
+            StartCoroutine(timerss());
+
         }
     }
+    IEnumerator timerss()
+    {
+
+
+        yield return new WaitForSeconds(0.2f);
+        Tracking.moves = Tracking.maxMoves;
+        Moves.text = Tracking.moves.ToString();
+        hasSpawnedShadow = false;
+    }
+
 }
--- Assets/Pontus/code/sadow_Mov.cs
+++ Assets/Pontus/code/sadow_Mov.cs
@@ -5,128 +5,161 @@ using UnityEngine;
 public class sadow_Mov : MonoBehaviour
 {
     private List<int> shadowList;
-    private int thisShadowOrder = Tracking.shadowOrder;
     public GameObject Shadow;
-    private float xPos;
-    private float yPos;
-    private bool canMove = true;
+    private Vector2 laserV2;
+    private Vector2 chestLaserV2;
 
+     GameObject Chest;
+    private float ChestxPos;
+    private float ChestyPos;
+    private bool canStart = false;
+    private Vector2 startPos;
+    private bool hasTriggered = false;
 
     private void Start()
     {
         shadowList = new List<int>(Tracking.tracking);
         Tracking.tracking.Clear();
+        Chest = GameObject.FindGameObjectWithTag("Chest");
+        startPos = Shadow.transform.position;
+        canStart = true;
 
-        StartCoroutine(PlayShadowMoves());
+
+
+
     }
 
+    private void Update()
+    {
+        if(Tracking.moves == 0 &&  canStart && !hasTriggered) 
+        {
+            hasTriggered = true;
+            Debug.Log("o skibidi wherew have you goon my name is ohio and  i somting to goon on o skibbidi...");
+            Shadow.transform.position = startPos;
+            Chest.transform.position = startPos;
+            StartCoroutine(PlayShadowMoves());
+
+            canStart = false;
+            
+        }
+    }
     IEnumerator PlayShadowMoves()
     {
         for (int i = 0; i < shadowList.Count; i++)
         {
             float xPos = Mathf.RoundToInt(Shadow.transform.position.x);
             float yPos = Mathf.RoundToInt(Shadow.transform.position.y);
-
             int move = shadowList[i];
 
-            // Apply movement based on recorded direction
+            ChestxPos = Mathf.RoundToInt(Chest.transform.position.x);
+            ChestyPos = Mathf.RoundToInt(Chest.transform.position.y);
+
             switch (move)
             {
                 case 1: // Up
-                    Shadow.transform.position = new Vector2(xPos, yPos + 1);
-                    Shadow.transform.rotation = Quaternion.Euler(0, 0, 0);
-                    break;
-                case 2: // Down
-                    Shadow.transform.position = new Vector2(xPos, yPos - 1);
-                    Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
-                    break;
-                case 3: // Right
-                    Shadow.transform.position = new Vector2(xPos + 1, yPos);
-                    Shadow.transform.rotation = Quaternion.Euler(0, 0, 270);
-                    break;
-                case 4: // Left
-                    Shadow.transform.position = new Vector2(xPos - 1, yPos);
-                    Shadow.transform.rotation = Quaternion.Euler(0, 0, 90);
-                    break;
-            }
+                    laserV2 = new Vector2(xPos, yPos + 1);
+                    Collider2D hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
 
-            yield return new WaitForSeconds(0.5f); // Wait between moves
-        }
-    }
-}
+                    if (hit != null && hit.CompareTag("Chest"))
+                    {
+                        chestLaserV2 = new Vector2(xPos, yPos + 2);
+                        Collider2D chestHitWall = Physics2D.OverlapPoint(chestLaserV2/*, LayerMask.GetMask("ground")*/);
 
+                        if (chestHitWall == null || !chestHitWall.CompareTag(Tags.Wall))
+                        {
+                            Shadow.transform.position = new Vector2(xPos , yPos +1);
+                            Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
+                            Chest.transform.position = new Vector2(ChestxPos , ChestyPos + 1);
+                            Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                        }
+                    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /* private void Update()
-    {
-        xPos = Mathf.RoundToInt(Shadow.transform.position.x);
-        yPos = Mathf.RoundToInt(Shadow.transform.position.y);
-
-        if (/*thisShadowOrder == Tracking.currentShadow true ) 
-        {
-            Debug.Log(shadowList.Count);
-            for(int i = 0; i < shadowList.Count; i++) 
-            {
-                if (canMove) 
-                {
-                    canMove = false;
-                    if (shadowList[i] == 1)
+                    else if (hit == null || !hit.CompareTag(Tags.Wall))
                     {
                         Shadow.transform.position = new Vector2(xPos, yPos + 1);
-                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 270);
                     }
-                    else if (shadowList[i] == 2)
+                    break;
+
+                case 2: // Down
+                    laserV2 = new Vector2(xPos, yPos - 1);
+                    hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+                    if (hit != null && hit.CompareTag("Chest"))
+                    {
+                        chestLaserV2 = new Vector2(xPos, yPos - 2);
+                        Collider2D chestHitWall = Physics2D.OverlapPoint(chestLaserV2/*, LayerMask.GetMask("ground")*/);
+
+                        if (chestHitWall == null || !chestHitWall.CompareTag(Tags.Wall))
+                        {
+                            Shadow.transform.position = new Vector2(xPos , yPos - 1);
+                            Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
+                            Chest.transform.position = new Vector2(ChestxPos, ChestyPos - 1);
+                            Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                        }
+                    }
+
+                    else if (hit == null || !hit.CompareTag(Tags.Wall))
                     {
                         Shadow.transform.position = new Vector2(xPos, yPos - 1);
-                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 90);
                     }
-                    else if (shadowList[i] == 3)
+                    break;
+
+                case 3: // Right
+                    laserV2 = new Vector2(xPos + 1, yPos);
+                    hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+                    if (hit != null && hit.CompareTag("Chest"))
+                    {
+                        chestLaserV2 = new Vector2(xPos + 2, yPos);
+                        Collider2D chestHitWall = Physics2D.OverlapPoint(chestLaserV2/*, LayerMask.GetMask("ground")*/);
+
+                        if (chestHitWall == null || !chestHitWall.CompareTag(Tags.Wall))
+                        {
+                            Shadow.transform.position = new Vector2(xPos + 1, yPos);
+                            Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
+                            Chest.transform.position = new Vector2(ChestxPos + 1, ChestyPos);
+                            Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+
+                        }
+                    }
+
+                    else if (hit == null || !hit.CompareTag(Tags.Wall))
                     {
                         Shadow.transform.position = new Vector2(xPos + 1, yPos);
                         Shadow.transform.rotation = Quaternion.Euler(0, 0, 0);
                     }
-                    else if (shadowList[i] == 4)
+                    break;
+
+                case 4: // Left
+                    laserV2 = new Vector2(xPos - 1, yPos);
+                    hit = Physics2D.OverlapPoint(laserV2/*, LayerMask.GetMask("ground")*/);
+
+                    if (hit != null && hit.CompareTag("Chest"))
+                    {
+                        chestLaserV2 = new Vector2(xPos - 2, yPos);
+                        Collider2D chestHitWall = Physics2D.OverlapPoint(chestLaserV2/*, LayerMask.GetMask("ground")*/);
+
+                        if (chestHitWall == null || !chestHitWall.CompareTag(Tags.Wall))
+                        {
+                            Shadow.transform.position = new Vector2(xPos - 1, yPos);
+                            Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
+                            Chest.transform.position = new Vector2(ChestxPos - 1, ChestyPos);
+                            Chest.transform.rotation = Quaternion.Euler(0, 0, 180);
+                        }
+                    }
+
+                    else if (hit == null || !hit.CompareTag(Tags.Wall)) 
                     {
                         Shadow.transform.position = new Vector2(xPos - 1, yPos);
-                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        Shadow.transform.rotation = Quaternion.Euler(0, 0, 180);
                     }
-                    StartCoroutine( WaitForXseconds());
-                    canMove = true;
-                }
+                    break;
             }
-            Tracking.currentShadow = thisShadowOrder++;
+            yield return new WaitForSeconds(0.5f);
         }
+        canStart = true;
+        hasTriggered = false;
     }
-    IEnumerator WaitForXseconds() 
-    {
-        yield return new WaitForSeconds(2f);
-
-    }*/
-
+}
