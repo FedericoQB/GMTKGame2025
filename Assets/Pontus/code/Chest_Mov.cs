e1f17549e363cd898d99ac2f69279a415079a97f
POKL67 <PONTUS.KLINT@GMAIL.COM>
2025-08-01 18:35:56 +0200

    bdgbfbd

--- Assets/Pontus/code/Chest_Mov.cs
+++ Assets/Pontus/code/Chest_Mov.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public static class Chester 
+{
+    public static Vector2 chestStartPosision;
+}
 public class Chest_Mov : MonoBehaviour
 {
-
+    private void Start()
+    {
+        Chester.chestStartPosision = gameObject.transform.position;
+    }
+    private void Update()
+    {
+        if (Tracking.reset)
+            gameObject.transform.position = Chester.chestStartPosision;
+    }
 }
