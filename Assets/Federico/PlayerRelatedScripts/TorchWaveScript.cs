eafc45efae1ad68e9d203ed9e17d98844276ef15
FedericoQB <federico.quijano@outlook.com>
2025-08-02 20:34:27 +0200

    Added colliders and Torch
    If this version crashes. its because of the Torch

---
+++ Assets/Federico/PlayerRelatedScripts/TorchWaveScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TorchWaveScript : MonoBehaviour
+{
+    public GameObject torchObject;
+
+    Image image;
+
+    public List<Sprite> spritesInOrder = new List<Sprite>();
+
+    int moveValue = 0;
+
+    [SerializeField] private float waitForSeconds = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (torchObject == null)
+        {
+            torchObject = gameObject;
+        }
+
+        image = torchObject.GetComponent<Image>();
+
+        spritesInOrder.Reverse();
+
+        StartCoroutine(AnimateTorch());
+    }
+
+    private IEnumerator AnimateTorch()
+    {
+        while (true)
+        {
+            image.sprite = spritesInOrder[moveValue];
+
+            moveValue++;
+            if (moveValue >= spritesInOrder.Count)
+            {
+                moveValue = 0;
+            }
+
+            yield return new WaitForSecondsRealtime(waitForSeconds);
+        }
+    }
+}
