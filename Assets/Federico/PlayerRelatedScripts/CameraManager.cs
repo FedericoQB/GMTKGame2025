a2027f0f33b13a7c0608cfc9803dc841f8b0e292
FedericoQB <federico.quijano@outlook.com>
2025-08-02 19:49:39 +0200

    Added multiple levels. Camera managers

--- Assets/Federico/PlayerRelatedScripts/CameraManager.cs
+++ Assets/Federico/PlayerRelatedScripts/CameraManager.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraManager : MonoBehaviour
 {
-    Camera camera;
-    GameObject cameraObject;
-
-    public List<Transform> cameraPositions = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
     {
-        camera = Camera.main;
-        cameraObject = camera.gameObject;
-    }   
+
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            GetComponent<CameraScript>().enabled = true;
+        }
+    }
 }
